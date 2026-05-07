de151f6772f037a5932e6872d5333bfa17d1c2d2
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-07 23:38:33 +0300

    Создание конфигурации CategoryConfiguration

---
+++ Satur8.WPF/Infrastructure/EntityTypeConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Satur8.WPF.Domain.Models;
+
+namespace Satur8.WPF.Infrastructure.EntityTypeConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder>)
+        {
+            builder.HasKey(e => e.CategoryId).HasName("categories_pkey");
+
+            builder.ToTable("categories");
+
+            builder.HasIndex(e => e.Name, "validate_categories_name").IsUnique();
+
+            builder.Property(e => e.CategoryId).HasColumnName("category_id");
+            builder.Property(e => e.Name)
+                .HasMaxLength(50)
+                .HasColumnName("name");
+        }
+    }
+}
