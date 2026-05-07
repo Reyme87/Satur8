b166af5fc88e0773cd537cf9698a8a63b1b85a8f
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-07 23:40:33 +0300

    Создание контекста БД SaturatorDbContext

---
+++ Satur8.WPF/Infrastructure/SaturatorDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Satur8.WPF.Domain.Models;
+using Satur8.WPF.Infrastructure.EntityTypeConfiguration;
+using Satur8.WPF.Infrastructure.Interfaces;
+
+namespace Satur8.WPF.Infrastructure
+{
+    internal class SaturatorDbContext : DbContext, ISaturatorDbContext
+    {
+        public SaturatorDbContext(DbContextOptions<SaturatorDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Preset> Presets { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Favourite> Favourites { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new PresetConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new FavouriteConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
