57e3b78aab003070328f0c1482704f028d4cb295
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-07 23:40:01 +0300

    Создание интерфейса ISaturatorDbContext

---
+++ Satur8.WPF/Infrastructure/Interfaces/ISaturatorDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Satur8.WPF.Domain.Models;
+
+namespace Satur8.WPF.Infrastructure.Interfaces
+{
+    public interface ISaturatorDbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Preset> Presets { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Favourite> Favourites { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
