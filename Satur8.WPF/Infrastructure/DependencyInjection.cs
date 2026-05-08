4e15289c7ea1996b9701118e87d9e9543ad81bcb
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-08 15:37:44 +0300

    Создание класса DependencyInjection с методом AddPersistence

---
+++ Satur8.WPF/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Satur8.WPF.Infrastructure.Interfaces;
+
+namespace Satur8.WPF.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<SaturatorDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+            services.AddScoped<ISaturatorDbContext>(provider => provider.GetRequiredService<SaturatorDbContext>());
+
+            return services;
+        }
+    }
+}
