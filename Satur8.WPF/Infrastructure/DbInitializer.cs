634306c32197cb9e4fd434f8f3b25a0c922327cb
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-08 18:58:03 +0300

    Создание класса DbInitializer

---
+++ Satur8.WPF/Infrastructure/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Satur8.WPF.Infrastructure
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SaturatorDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
