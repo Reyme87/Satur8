5d1ebe2c5287188b59109837e7bd57fec197aaa5
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-10 14:40:59 +0300

    Создание фабрики контекста SaturatorDbContextFactory

---
+++ Satur8.Persistence/ContextFactory/SaturatorDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Satur8.Persistence.ContextFactory
+{
+    public class SaturatorDbContextFactory : IDesignTimeDbContextFactory<SaturatorDbContext>
+    {
+        public SaturatorDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "../Satur8.WPF");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var connectionString =
+            configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder =
+                new DbContextOptionsBuilder<SaturatorDbContext>();
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new SaturatorDbContext(optionsBuilder.Options);
+        }
+    }
+}
