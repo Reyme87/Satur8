6473d25941315e28889b0bfae4dd4af88d3d44ab
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-10 19:50:44 +0300

    Создание unit-тестов для Category

---
+++ Satur8.Tests/Models/Categories/CategoryCrudTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Satur8.Domain.Models;
+using Satur8.Tests.Common;
+
+namespace Satur8.Tests.Models.Categories
+{
+    public class CategoryCrudTests : TestCrudBase
+    {
+        [Fact]
+        public async Task CreateCategory_Success()
+        {
+            // Arrange
+            var category = new Category
+            {
+                CategoryId = Guid.NewGuid(),
+                Name = "NewCategory"
+            };
+
+            // Act
+            await Context.Categories.AddAsync(category);
+            await Context.SaveChangesAsync();
+
+            // Assert
+            var saved = await Context.Categories.FirstOrDefaultAsync(c => c.Name == "NewCategory");
+            Assert.NotNull(saved);
+            Assert.Equal(category.CategoryId, saved.CategoryId);
+        }
+
+        [Fact]
+        public async Task ReadCategory_ReturnCorrectData()
+        {
+            // Arrange
+            // Act
+            var category = await Context.Categories.FindAsync(SaturatorContextFactory.CategoryToDeleteId);
+
+            // Assert
+            Assert.NotNull(category);
+            Assert.Equal("DeleteCategory", category.Name);
+        }
+
+        [Fact]
+        public async Task UpdateCategory_UpdateSuccessfully()
+        {
+            // Arrange
+            var category = await Context.Categories.FindAsync(SaturatorContextFactory.CategoryToUpdateId);
+            var name = "NewName";
+
+            // Act
+            category.Name = name;
+            await Context.SaveChangesAsync();
+
+            // Assert
+            var updated = await Context.Categories.FirstOrDefaultAsync(c => c.CategoryId == SaturatorContextFactory.CategoryToUpdateId);
+            Assert.Equal(name, category.Name);
+        }
+
+        [Fact]
+        public async Task DeleteCategory_DeleteSuccessfully()
+        {
+            // Arrange
+            var category = await Context.Categories.FirstOrDefaultAsync(c => c.CategoryId == SaturatorContextFactory.CategoryToDeleteId);
+
+            // Act
+            Context.Categories.Remove(category!);
+            await Context.SaveChangesAsync();
+
+            // Assert
+            var deleted = await Context.Categories.FindAsync(SaturatorContextFactory.CategoryToDeleteId);
+            Assert.Null(deleted);
+        }
+    }
+}
