a5496bf0aa6a00bc8a023a595e1dc9b71a88ff6e
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-10 14:42:37 +0300

    Создание миграций БД

---
+++ Satur8.Persistence/Migrations/20260510114032_InitialCreate.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Satur8.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    category_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("categories_pkey", x => x.category_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    login = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    password_hash = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pkey", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "presets",
+                columns: table => new
+                {
+                    preset_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    description = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: true),
+                    category_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    parameters_json = table.Column<string>(type: "json", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("presets_pkey", x => x.preset_id);
+                    table.ForeignKey(
+                        name: "presets_category_id_fkey",
+                        column: x => x.category_id,
+                        principalTable: "categories",
+                        principalColumn: "category_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "presets_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "favourites",
+                columns: table => new
+                {
+                    favourites_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    preset_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("favourites_pkey", x => x.favourites_id);
+                    table.ForeignKey(
+                        name: "favourites_preset_id_fkey",
+                        column: x => x.preset_id,
+                        principalTable: "presets",
+                        principalColumn: "preset_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "favourites_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "validate_categories_name",
+                table: "categories",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_favourites_preset_id",
+                table: "favourites",
+                column: "preset_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_favourites_user_id",
+                table: "favourites",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_presets_category_id",
+                table: "presets",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_presets_user_id",
+                table: "presets",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "favourites");
+
+            migrationBuilder.DropTable(
+                name: "presets");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
