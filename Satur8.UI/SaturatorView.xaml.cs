cde58f07c3b6706970dc140836d3293f038710f0
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-17 23:05:46 +0300

    Создание базового интерфейса плагина

---
+++ Satur8.UI/SaturatorView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace Satur8.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для SaturatorView.xaml
+    /// </summary>
+    public partial class SaturatorView : UserControl
+    {
+        public SaturatorView()
+        {
+            InitializeComponent();
+            DataContext = new SaturatorViewModel();
+        }
+
+    }
+}
