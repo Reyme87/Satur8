b5540210517c817c8db0f2c6ec5cb8f030f5b410
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-21 21:49:29 +0300

    Создание конвертера BoolToVisibility

---
+++ Satur8.UI/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Satur8.UI.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Visibility)value == Visibility.Visible;
+        }
+    }
+}
