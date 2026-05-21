8f374de40b08bcf3e8fd6838e6e174ecf0c8b36e
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-21 21:39:16 +0300

    Изменение SaturatorViewModel

--- Satur8.UI/SaturatorViewModel.cs
+++ Satur8.UI/SaturatorViewModel.cs
@@ -15,33 +15,82 @@ namespace Satur8.UI
         }
 
         private double _driveValue = 1.0;
-        public double DriveValue { get => _driveValue; set => Set(ref _driveValue, value); }
-
         private double _outputGainValue = 1.0;
-        public double OutputGainValue { get => _outputGainValue; set => Set(ref _outputGainValue, value); }
-
         private double _evenAmountValue = 0.5;
-        public double EvenAmountValue { get => _evenAmountValue; set => Set(ref _evenAmountValue, value); }
-
         private double _oddAmountValue = 1.0;
-        public double OddAmountValue { get => _oddAmountValue; set => Set(ref _oddAmountValue, value); }
-
         private double _biasValue = 0.0;
-        public double BiasValue { get => _biasValue; set => Set(ref _biasValue, value); }
-
         private int _clipTypeIndex = 0;
-        public int ClipTypeIndex { get => _clipTypeIndex; set => Set(ref _clipTypeIndex, value); }
-
         private double _mixPercent = 100.0;
-        public double MixPercent { get => _mixPercent; set => Set(ref _mixPercent, value); }
-
         private double _thresholdDb = -12.0;
-        public double ThresholdDb { get => _thresholdDb; set => Set(ref _thresholdDb, value); }
-
         private double _ratioValue = 4.0;
-        public double RatioValue { get => _ratioValue; set => Set(ref _ratioValue, value); }
-
         private bool _autoGainEnabled = false;
-        public bool AutoGainEnabled { get => _autoGainEnabled; set => Set(ref _autoGainEnabled, value); }
+        private bool _showAccountPanel;
+
+
+        public double DriveValue 
+        { 
+            get => _driveValue; 
+            set => Set(ref _driveValue, value); 
+        }
+
+        public double OutputGainValue 
+        { 
+            get => _outputGainValue; 
+            set => Set(ref _outputGainValue, value); 
+        }
+
+        public double EvenAmountValue 
+        { 
+            get => _evenAmountValue; 
+            set => Set(ref _evenAmountValue, value); 
+        }
+
+        public double OddAmountValue 
+        { 
+            get => _oddAmountValue; 
+            set => Set(ref _oddAmountValue, value); 
+        }
+
+        public double BiasValue 
+        { 
+            get => _biasValue; 
+            set => Set(ref _biasValue, value); 
+        }
+
+        public int ClipTypeIndex 
+        { 
+            get => _clipTypeIndex; 
+            set => Set(ref _clipTypeIndex, value); 
+        }
+
+        public double MixPercent 
+        { 
+            get => _mixPercent; 
+            set => Set(ref _mixPercent, value);
+        }
+
+        public double ThresholdDb 
+        { 
+            get => _thresholdDb; 
+            set => Set(ref _thresholdDb, value); 
+        }
+
+        public double RatioValue 
+        { 
+            get => _ratioValue; 
+            set => Set(ref _ratioValue, value); 
+        }
+
+        public bool AutoGainEnabled 
+        { 
+            get => _autoGainEnabled; 
+            set => Set(ref _autoGainEnabled, value); 
+        }
+
+        public bool ShowAccountPanel
+        {
+            get => _showAccountPanel;
+            set => Set(ref _showAccountPanel, value);
+        }
     }
 }
