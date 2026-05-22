4f4d98c69df6627b6d4d1eab2085569737173c3e
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-22 16:01:05 +0300

    Создание класса PluginService

---
+++ Satur8.Persistence/Services/PluginService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Satur8.Persistence.Services
+{
+    public static class PluginService
+    {
+        public static IServiceProvider Services { get; private set; } = null!;
+
+        public static void Initialize()
+        {
+            if (Services != null)
+            {
+                return;
+            }
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+            var services = new ServiceCollection();
+
+            services.AddPersistence(configuration);
+
+            Services = services.BuildServiceProvider();
+
+            using (var scope = Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<SaturatorDbContext>();
+
+                DbInitializer.Initialize(db);
+            }
+        }
+    }
+}
