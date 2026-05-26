8a54e6c433d9b3085df091c94645cf6e1ba22269
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-26 20:06:17 +0300

    Создание конвертеров InverseBool и StringToVisibility

---
+++ Satur8.UI/Converters/InverseBoolConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Satur8.UI.Converters
+{
+    public class InverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type t, object p, CultureInfo c)
+            => value is bool b && !b;
+
+        public object ConvertBack(object v, Type t, object p, CultureInfo c)
+            => v is bool b && !b;
+    }
+}
---
+++ Satur8.UI/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Satur8.UI.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type t, object p, CultureInfo c)
+            => value is string s && !string.IsNullOrEmpty(s)
+                ? Visibility.Visible : Visibility.Collapsed;
+
+        public object ConvertBack(object v, Type t, object p, CultureInfo c)
+            => throw new NotSupportedException();
+    }
+}
