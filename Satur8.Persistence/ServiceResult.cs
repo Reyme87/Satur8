382dfe0e934770890e9fb164d35bc3ecb86c5681
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-28 00:49:39 +0300

    Создание класса ServiceResult

---
+++ Satur8.Persistence/ServiceResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Satur8.Persistence
+{
+    public class ServiceResult
+    {
+        public bool Success { get; private set; }
+        public string? Error { get; private set; }
+
+        public static ServiceResult Ok() => new() { Success = true };
+        public static ServiceResult Fail(string e) => new() { Success = false, Error = e };
+    }
+}
