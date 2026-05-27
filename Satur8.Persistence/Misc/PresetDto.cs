e6953057807a911794b0ba1ee796acd9274162b1
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-28 00:48:41 +0300

    Создание классов PresetDto и PresetParameters

---
+++ Satur8.Persistence/Misc/PresetDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Satur8.Persistence.Misc
+{
+    public class PresetDto
+    {
+        public Guid PresetId { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string CategoryName { get; set; } = "";
+        public string AuthorLogin { get; set; } = "";
+        public bool IsFavourite { get; set; }
+        public PresetParameters Parameters { get; set; } = new();
+    }
+}
---
+++ Satur8.Persistence/Misc/PresetParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace Satur8.Persistence.Misc
+{
+    public class PresetParameters
+    {
+        public double Drive { get; set; }
+        public double Output { get; set; }
+        public double Even { get; set; }
+        public double Odd { get; set; }
+        public double Bias { get; set; }
+        public double Mix { get; set; }
+        public double Threshold { get; set; }
+        public double Ratio { get; set; }
+    }
+}
